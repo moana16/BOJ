7d4ecf41dab5f4bfec4b58691f5dc2b7dbcf430f
jimin <moana16@naver.com>
2024-03-13 16:01:15 +0900

    0313

--- 문자열/한국이_그리울_땐_서버에_접속하지.cpp
+++ 문자열/한국이_그리울_땐_서버에_접속하지.cpp
@@ -1,24 +1,20 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-int main () {
-    int n; cin>>n;
+int main() {
+    ios::sync_with_stdio(false); cin.tie(0);
+    int N; cin>>N;
     string pattern; cin>>pattern;
-
-    auto star = pattern.find("*");
-
-    string start = pattern.substr(0, star);
-    string end = pattern.substr(star + 1);
-    
-    while(n--) {
+    string st, en;
+    auto it = pattern.find('*');
+    st = pattern.substr(0,it);
+    en = pattern.substr(it+1);
+    while(N--) {
         string s; cin>>s;
-
-        if(s.length() < start.length() + end.length()) {
-            cout<<"NE\n";
-            continue;
-        }
-        if(s.substr(0,start.length()) == start && s.substr(s.length()-end.length()) == end && s.length() >= start.length() + end.length()) cout<<"DA\n";
+        if(s.size() >= st.size() + en.size() &&s.substr(0,st.size()) == st && s.substr(s.size()-en.size()) == en) cout<<"DA\n";
         else cout<<"NE\n";
+        
+
+
     }
-    
 }
