3a536b2715ee303e15fb831ebdebf0e8de029c1a
jimin <moana16@naver.com>
2024-04-18 13:33:53 +0900

    0418

--- Deque/AC.cpp
+++ Deque/AC.cpp
@@ -1,55 +1,60 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-void sol() {
-    string s; cin>>s; 
-    int n; cin>>n; 
-    deque<int> dq;
-    string arr; cin>>arr;
-    string arr2="";
-    int rev = 0;
-    
-    for(auto c : arr) {
-        if(isdigit(c)) {
-            arr2 += c;
-        }
-        else {
-            if(!arr2.empty()) {
-                dq.push_back(stoi(arr2));
-                arr2 = "";
-            }
-        }
-    }
-    for(auto c : s) {
-        
-      if(c == 'R')
-        rev = 1 - rev;
-      else{
-        if(dq.empty()){
-            cout << "error\n";
-            return;
-        }
-        if(!rev) dq.pop_front();
-        else dq.pop_back();
-      }
-    }
-    
-      if(rev) reverse(dq.begin(), dq.end());
-      
-    int sz = dq.size();
-    cout<<"[";
-    while(sz--) {
-        cout<<dq.front();
-        dq.pop_front();
-        if(sz != 0) cout<<",";
-    }
-    cout<<"]\n"; 
-}
-
 int main() {
     ios::sync_with_stdio(false); cin.tie(0);
-    int t; cin>>t;
+    int t,n; cin>>t;
+    string p;
+    
     while(t--) {
-        sol();
+        cin>>p>>n;
+        deque<int> dq;
+        string arr; cin>>arr;
+        bool flag = false, error = false;
+        string num ="";
+        for(auto c : arr) {
+            if(isdigit(c)) num += c;
+            else if(num.size() != 0 ) {
+                dq.push_back(stoi(num));
+                num = "";
+
+            }
+        }
+
+        for(auto c : p) {
+            if(c == 'R') { 
+                flag = !flag;
+            }
+            else {
+                if(dq.empty()) {    
+                    error = true;
+                    break;
+                }
+                if(!flag) dq.pop_front();
+                else dq.pop_back();
+
+            }
+        }
+        if(error) cout<<"error\n";
+        else if(!error && dq.empty()) cout<<"[]\n";
+        else if(!error && !dq.empty() ) {
+            cout<<'[';
+            if(flag) {
+                while(!dq.empty()) {
+                    cout<<dq.back();
+                    dq.pop_back();
+                    if(!dq.empty()) cout<<",";
+                }
+                
+            }
+            else {
+                while(!dq.empty()) {
+                    cout<<dq.front();
+                    dq.pop_front();
+                    if(!dq.empty()) cout<<",";
+                }
+            }
+            cout<<"]\n";
+        }
     }
 }
