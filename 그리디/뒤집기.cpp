cefe3e962798e227fc82b7a9266810a1e7e6f4ca
jimin <moana16@naver.com>
2024-04-30 16:05:21 +0900

    0430

--- 그리디/뒤집기.cpp
+++ 그리디/뒤집기.cpp
@@ -1,15 +1,12 @@
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
 using namespace std;
 
-string s;
-int cnt[2] = {0,0};
-
 int main() {
-    cin >> s;
-    cnt[s[0]-'0']++;
-    for(int i = 1; i<s.length(); i++) {
-        if(s[i] != s[i-1]) cnt[s[i]-'0']++;
+    string str; cin>>str;
+    int cnt[2] = {0,0};
+    cnt[str[0]-'0']++;
+    for(int i=1; i<str.size(); i++) {
+        if(str[i] != str[i-1]) cnt[str[i] - '0']++;
     }
-    cout<<min(cnt[0],cnt[1]);
-
+    cout<<min(cnt[0], cnt[1]);
 }
