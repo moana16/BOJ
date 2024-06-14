c186e6a1f8fcc13c2f530e15c9c91986109c8efc
jimin <moana16@naver.com>
2024-06-14 14:42:25 +0900

    0614

--- 백트래킹/N과_M_1.cpp
+++ 백트래킹/N과_M_1.cpp
@@ -1,35 +1,27 @@
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
 using namespace std;
-// 1부터 N까지 자연수 중에서 중복 없이 M개를 고른 수열
-// 자릿수와 숫자를 따로 생각하면서 해당 숫자가 사용되었는지 파악하면서......재귀 호출
-int n, m;
-int arr[10];
-bool isUsed[10];
 
-void sol(int k)
-{
-    if (k == m)
-    {
-        for (int i = 0; i < m; i++)
-            cout << arr[i] << " ";
-        cout << '\n';
+int N,M;
+
+void sol(vector<int> nums, int cur) {
+    if(nums.size ()== M) {
+        for(auto c : nums) cout<<c<<" ";
+        cout<<"\n";
         return;
     }
-    for (int i = 1; i <= n; i++)
-    {
-        if (!isUsed[i])
-        {
-            arr[k] = i;
-            isUsed[i] = true;
-            sol(k + 1);
-            isUsed[i] = false;
+
+    for(int i=1; i<=N; i++) {
+        if(find(nums.begin(), nums.end(), i) == nums.end()) {
+            nums.push_back(i);
+            sol(nums, i);
+            nums.pop_back();
         }
     }
 }
 
-int main()
-{
-    ios::sync_with_stdio(false); cin.tie(0);
-    cin >> n >> m;
-    sol(0);
+
+int main() {
+    cin>>N>>M;
+    vector<int> nums;
+    sol(nums, 0);
 }
