0312a8a8d6f716c9a238d79a358bdb4f40f309e1
jimin <moana16@naver.com>
2025-01-06 15:48:49 +0900

    0106

--- 이분탐색/수_찾기.cpp
+++ 이분탐색/수_찾기.cpp
@@ -1,19 +1,38 @@
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
 using namespace std;
-
-int N,M;
-int num[100003];
-
 
 int main() {
     ios::sync_with_stdio(false); cin.tie(0);
+    int N,M;
     cin>>N;
-    for(int i=0; i<N; i++) cin>>num[i];
-    sort(num,num+N);
+    vector<int> A(N);
+    for(int i=0; i<N; i++) cin>>A[i];
+
+    sort(A.begin(), A.end());
+
     cin>>M;
-    while(M--) {
-        int t; cin>>t;
-        cout<<binary_search(num,num+N,t)<<'\n';
-    }
     
+    while(M--) {
+        int x; cin>>x;
+        int s=0, e=N-1;
+
+        bool flag = false;
+
+        while(s <= e) {
+            int mid = s + (e - s)/2;
+            if(A[mid] <= x) {
+                if(A[mid] == x ) {
+                    flag = true;
+                    break;
+
+                }
+                s = mid+1;
+            }
+            else e = mid-1;
+        }
+
+        if(flag) cout<<1<<'\n';
+        else cout<<0<<'\n';
+
+    }
 }
