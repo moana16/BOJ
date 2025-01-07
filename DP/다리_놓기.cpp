54383df45cfd806dbf9bde37fc21073c63b20131
jimin <moana16@naver.com>
2025-01-07 17:16:51 +0900

    0107

--- DP/다리_놓기.cpp
+++ DP/다리_놓기.cpp
@@ -1,18 +1,21 @@
 #include<bits/stdc++.h>
 using namespace std;
-long long dp[33][33];
 
+long long dp[33][33];
 int main() {
     int T; cin>>T;
+
     dp[0][0] = 1;
     for(int i=1; i<=30; i++) {
         dp[i][0] = 1;
         for(int j=1; j<=30; j++) {
-            dp[i][j] = dp[i-1][j] + dp[i-1][j-1];
+            dp[i][j]= dp[i-1][j] + dp[i-1][j-1];
         }
     }
+
     while(T--) {
-        int n,m; cin>>n>>m;
-        cout<<dp[m][n]<<'\n';
+        int N,M; cin>>N>>M;
+        cout<<dp[M][N]<<'\n';
+
     }
 }
--- DP/피보나치_수_5.cpp
+++ DP/피보나치_수_5.cpp
@@ -1,11 +1,18 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+int fibo[50];
+
 int main() {
     int n; cin>>n;
-    int dp[23];
-    dp[0] = 0;
-    dp[1]= 1;
-    for(int i=2; i<=n; i++) dp[i] = dp[i-1]+dp[i-2];
-    cout<<dp[n];
+
+    fibo[0] = 0;
+    fibo[1] = 1;
+
+    for(int i=2; i<=n; i++) {
+        fibo[i] = fibo[i-2] + fibo[i-1];
+    }
+
+    cout<<fibo[n];
+
 }
