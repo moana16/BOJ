14b8783ab11486f2173036165b0581984a85c019
jimin <moana16@naver.com>
2025-01-10 14:33:07 +0900

    0110

--- DP/Four_Squares.cpp
+++ DP/Four_Squares.cpp
@@ -6,14 +6,10 @@ int main() {
     vector<int> dp(n+1, INT_MAX);
     dp[0] = 0;
 
-    for(int i=1; i<=sqrt(n); i++) { //n 이하의 모든 제곱수 확인
-        int square = i*i;
-        for(int j=square; j<=n; j++) {
-            if(dp[j-square] != INT_MAX) {
-                dp[j] = min(dp[j], dp[j-square]+1);
-            }
+    for(int i=1; i<=n; i++) {
+        for(int j=1; j*j<=i; j++) {
+            dp[i] = min(dp[i], dp[i-j*j]+1);
         }
     }
     cout<<dp[n];
-
 }
--- DP/조합.cpp
+++ DP/조합.cpp
@@ -1,43 +1,37 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-int main(){
-    int n, m;
-    cin >> n >> m;
- 
-    string dp[102][102];
+typedef vector<int> BigInt;
+BigInt dp[103][103];
+
+BigInt add(BigInt a, BigInt b) {
+    BigInt res;
+    int carry = 0,sum = 0;
+    int size = max(a.size(), b.size());
+
+    for(int i=0; i<size || carry; i++) {
+        if(i < a.size()) sum += a[i];
+        if(i < b.size()) sum += b[i];
+        sum += carry;
+        res.push_back(sum%10);
+        carry = sum / 10;
+        sum = 0;
+    }
+    return res;
+}
+
+
+int main() {
+    int n,m; cin>>n>>m;
     
-    for (int i = 1; i <= n; i++){
-        for (int j = 0; j <= i; j++){
-            if(j == 0 || j == i)
-                dp[i][j] = "1"; // m = 0 or n 이면 1
-            else{
-                int sum = 0; // 두 문자열의 한 자리를 더한 값
-                string n1 = dp[i - 1][j]; // 더 할 문자열1
-                string n2 = dp[i - 1][j - 1]; // 더 할 문자열2
- 
-                // 두 문자열 수를 더하기
-                // 두 문자열을 모두 처리하고 남은 자릿수 sum까지 처리했으면 종료 
-                while(!n1.empty() || !n2.empty() || sum){
-                    if(!n1.empty()){  // 비어있으면 처리할 필요없음.
-                        sum += n1.back() - '0'; // 한 자리 더해줌
-                        n1.pop_back(); // 한 자리 제거
-                    }
-                    if(!n2.empty()){ // 비어있으면 처리할 필요없음.
-                        sum += n2.back() - '0'; // 한 자리 더해줌
-                        n2.pop_back(); // 한 자리 제거 
-                    }
-                    // sum의 값 중 일의 자릿수 만 넣어주어함.
-                    // 10의 나머지를 push해주고
-                    dp[i][j].push_back((sum%10) + '0');
-                    sum /= 10; // 10의 몫을 저장해주면 다음 자리수가 남음
-                }
-                // 위 방식대로 했다면 수가 일의 자리부터 시작됨.
-                // 반대로 뒤집어주어서 답을 구해줌.
-                reverse(dp[i][j].begin(), dp[i][j].end());
-            }
+    dp[0][0] = {1};
+    for(int i=1; i<=100; i++) {
+        dp[i][0] = {1};
+        for(int j=1; j<=100; j++) {
+            dp[i][j] = add(dp[i-1][j], dp[i-1][j-1]);
         }
     }
-    cout << dp[n][m];
-    return 0;   
+
+    BigInt res = dp[n][m];
+    for(auto it = res.rbegin(); it != res.rend(); it++) cout<<*it;
 }
