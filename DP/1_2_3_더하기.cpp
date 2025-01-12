95605c3a381577682082b8ad1f845d6bd0f68efd
jimin <moana16@naver.com>
2025-01-12 14:45:54 +0900

    0112

--- DP/1_2_3_더하기.cpp
+++ DP/1_2_3_더하기.cpp
@@ -1,21 +1,20 @@
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
 using namespace std;
 
-int arr[10];
-
 int main() {
-    int T; cin>>T;
-    arr[1] = 1;
-    arr[2] = 2;
-    arr[3] = 4;
-    for(int i = 4 ; i <= 10; i++ ) {
-        arr[i] = arr[i-1] + arr[i-2] + arr[i-3];
+    int t; cin>>t;
 
+    int dp[13];
+    dp[1] = 1;
+    dp[2] = 2;
+    dp[3] = 4;
+    
+    for(int i=4; i<=11; i++) {
+        dp[i] = dp[i-1] + dp[i-2] + dp[i-3];
     }
-
-    while(T--) {
-        int x; cin>>x;
-        cout<<arr[x]<<'\n';
+    while(t--) {
+        int n; cin>>n;
+        cout<<dp[n]<<'\n';;
 
     }
 }
--- DP/2_n_타일링_2.cpp
+++ DP/2_n_타일링_2.cpp
@@ -1,16 +1,16 @@
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
 using namespace std;
 
-int N;
-int dp[1001];
-
 int main() {
-    cin>>N;
+    int n; cin>>n;
+    int dp[1003];
+
     dp[1] = 1;
     dp[2] = 3;
     dp[3] = 5;
-    for(int i=4; i<=N; i++) {
-        dp[i] = (dp[i-1] + dp[i-2]*2)%10007;
+
+    for(int i=4; i<=n; i++) {
+        dp[i] = (dp[i-1] + dp[i-2] * 2) % 10007;
     }
-    cout<<dp[N];
+    cout<<dp[n];
 }
