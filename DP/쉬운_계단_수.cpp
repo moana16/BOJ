d0c8bca036ee7c5300786a319912592c4583b81d
jimin <moana16@naver.com>
2025-01-16 16:09:04 +0900

    0116

--- DP/쉬운_계단_수.cpp
+++ DP/쉬운_계단_수.cpp
@@ -1,31 +1,27 @@
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
 using namespace std;
-
-constexpr long long DIV = 1000000000;
-long long dp[101][10];
-
-/*
-dp[n][i] = n번째 자리에 i가 올 수
-dp[3][3] = 3번째 자리에 3이 올 경우의 수 = 두번째 자리에 2가올 경우의 수 + 두번째 자리에 4가 올 경우의 수
-*/
-
+typedef long long ll;
 int main() {
-    int n; cin>>n;
-    fill(dp[1],dp[1]+10,1);
+    int N; cin>>N;
 
-    for(int i=2; i<=n; i++) {
-        for(int j=0; j<= 9; j++) {
-            if(j!=0) dp[i][j] += dp[i-1][j-1];
-            if(j!=9) dp[i][j] += dp[i-1][j+1];
-            dp[i][j] %= DIV;
+    ll dp[103][103];
+
+    for(int i=1; i<=9; i++) {
+        dp[1][i] = 1;
+    }
+
+    for(int i=2; i<=N; i++) {
+        for(int j=0; j<=9; j++) {
+            if(j > 0) dp[i][j] += dp[i-1][j-1];
+            if(j < 9) dp[i][j] += dp[i-1][j+1];
+            dp[i][j] %= 1000000000;
         }
     }
-    long long ans = 0;
-    for(int i=0; i<9; i++) {
-        ans += dp[n][i];
+
+    ll res = 0;
+    for(int j=0; j<=9; j++) {
+        res = (res + dp[N][j]) % 1000000000;
     }
-    ans %= DIV;
-    cout<<ans;
 
-
+    cout<<res;
 }
--- DP/징검다리_건너기.cpp
+++ DP/징검다리_건너기.cpp
@@ -1,33 +1,27 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-int N,K;
-vector<pair<int,int>> v;
-int dp[25][2];
-
 int main() {
-    cin>>N;
-    v.push_back({0,0});
-    for(int i=0; i<N-1; i++) {
-        int s,m; cin>>s>>m;
-        v.push_back({s,m});
-    }
+    int N,K; cin>>N;
+    vector<pair<int,int>> v(N);
+    v[0] = {0,0};
+    for(int i=1; i<N; i++) cin>>v[i].first>>v[i].second;
     cin>>K;
 
+    int dp[25][2];
     memset(dp, 0x7f7f7f, sizeof(dp));
 
-    dp[1][0] =  0;
+    dp[1][0] = 0;
     dp[2][0] = v[1].first;
     dp[3][0] = min(dp[2][0] + v[2].first, dp[1][0] + v[1].second);
 
     for(int i=4; i<=N; i++) {
-        dp[i][0] = min(dp[i-1][0] + v[i-1].first, dp[i-2][0] + v[i-2].second);
-
-        dp[i][1] = min({v[i-1].first+dp[i-1][1], v[i-2].second+dp[i-2][1], K+dp[i-3][0]});
+        dp[i][0] = min(dp[i-2][0] + v[i-2].second, dp[i-1][0] + v[i-1].first);
+        dp[i][1] = min({v[i-1].first + dp[i-1][1], v[i-2].second + dp[i-2][1], K+dp[i-3][0]});
     }
 
-
     cout<<min(dp[N][0], dp[N][1]);
+
 
 
 }
