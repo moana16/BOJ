a453a6f99510265d002996b963b07aa72d5a620f
jimin <moana16@naver.com>
2025-01-20 16:06:00 +0900

    0120

--- DP/구간_합_구하기_5.cpp
+++ DP/구간_합_구하기_5.cpp
@@ -1,29 +1,23 @@
 #include<bits/stdc++.h>
 using namespace std;
-
-int N,M;
-int mat[1030][1030];
-int dp[1030][1030];
+int mat[1050][105], dp[1050][1050];
 
 int main() {
     ios::sync_with_stdio(false); cin.tie(0);
-    cin>>N>>M;
+    int N,M; cin>>N>>M;
     for(int i=1; i<=N; i++) {
+        dp[i][0] = 0;
         for(int j=1; j<=N; j++) {
             cin>>mat[i][j];
-        }
-    }
-    for(int i=1; i<=N; i++) {
-        for(int j=1; j<=N; j++) {
             dp[i][j] = dp[i-1][j] + dp[i][j-1] - dp[i-1][j-1] + mat[i][j];
         }
     }
-    
-    for (int i=0; i<M; i++)
-    {
-        int x1, y1, x2, y2; 
+
+    int x1,y1,x2,y2;
+
+    for(int i=0; i<M; i++) {
         cin>>x1>>y1>>x2>>y2;
-        cout<<(dp[x2][y2]-dp[x2][y1-1])+(dp[x1-1][y1-1]-dp[x1-1][y2])<<'\n';
+        cout<<dp[x2][y2] - dp[x2][y1-1] + dp[x1-1][y1-1] - dp[x1-1][y2]<<'\n';
 
     }
 }
