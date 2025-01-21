197c3029c31c4a160687f70bd53515cb3f2c3e19
jimin <moana16@naver.com>
2025-01-21 14:54:55 +0900

    0121

--- DP/스티커.cpp
+++ DP/스티커.cpp
@@ -1,31 +1,29 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-
-int dp[2][100001];
-
 int main() {
-    int t; cin>>t;
-    while(t--) {
+    int T; cin>>T;
+    while(T--) {
         int n; cin>>n;
-        int arr[2][100001];
+        int mat[2][100003],dp[2][100003];
+
         for(int i=0; i<2; i++) {
             for(int j=0; j<n; j++) {
-                cin>>arr[i][j];
+                cin>>mat[i][j];
             }
         }
 
-        dp[0][0] = arr[0][0];
-        dp[1][0] = arr[1][0];
-        dp[0][1] = dp[1][0] + arr[0][1];
-        dp[1][1] = dp[0][0] + arr[1][1];
+        dp[0][0] = mat[0][0];
+        dp[1][0] = mat[1][0];
+        dp[0][1] = dp[1][0]+mat[0][1];
+        dp[1][1] = dp[0][0]+mat[1][1];
 
-        for(int i=2; i<n; i++) {
-            dp[0][i] = arr[0][i] + max(dp[1][i-1], dp[1][i-2]);
-            dp[1][i] = arr[1][i] + max(dp[0][i-1], dp[0][i-2]);
+        for(int j=2; j<n; j++) {
+            dp[0][j] = mat[0][j] + max(dp[1][j-1] , dp[1][j-2]);
+            dp[1][j] = mat[1][j] + max(dp[0][j-1], dp[0][j-2]);
         }
         cout<<max(dp[0][n-1], dp[1][n-1])<<'\n';
-}
+
 
     }
-    
+}
