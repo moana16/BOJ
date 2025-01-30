811bf11bb57a2f82cbddf21c2141ec3f744532ff
jimin <moana16@naver.com>
2025-01-30 15:23:03 +0900

    0130

---
+++ DP/병사_배치하기.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int N; cin>>N;
+    vector<int> v(N+1);
+    for(int i=1; i<=N; i++) cin>>v[i];
+
+    int dp[2002];
+
+    dp[0] = 0;
+    dp[1] = 1;
+    for(int i=2; i<=N; i++) {
+        dp[i] = 1;
+        for(int j=1; j<=i; j++) {
+            if(v[i] < v[j]) dp[i] = max(dp[i], dp[j]+1);
+        }
+    }
+
+    cout<<N-*max_element(dp+1, dp+N+1);
+}
--- DP/제곱수의_합.cpp
+++ DP/제곱수의_합.cpp
@@ -1,17 +1,24 @@
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
 using namespace std;
 
-int dp[100001];
+int dp[100005];
 
 int main() {
-    int n; cin>>n;
-    for(int i=1; i<=n; i++) {
-        dp[i] = i;
+    int N; cin>>N;
+
+    int MAX = (int)sqrt(100000);
+
+    for(int i=1; i<=MAX; i++) {
+        dp[i*i] = 1;
+    }
+
+    for(int i=2; i<=N; i++) {
+        dp[i] = dp[i-1]+1;
         for(int j=1; j*j<=i; j++) {
-            dp[i] = min(dp[i], dp[i-j*j]+1);
+            dp[i] = min(dp[i-j*j]+ 1, dp[i]);
         }
     }
 
-    cout<<dp[n];
+    cout<<dp[N];
 
 }
