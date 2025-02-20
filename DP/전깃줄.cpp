d4a8a0d5ffb1aabac733e7089f3946529eb3c43e
jimin <moana16@naver.com>
2025-02-20 15:44:56 +0900

    0220

--- DP/전깃줄.cpp
+++ DP/전깃줄.cpp
@@ -1,13 +1,12 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-int N;
-vector<pair<int,int>> v;
-int dp[103];
-
 int main() {
-    cin>>N;
+    int N; cin>>N;
+    vector<pair<int,int>> v;
+
     v.push_back({0,0});
+
     for(int i=1; i<=N; i++) {
         int a,b; cin>>a>>b;
         v.push_back({a,b});
@@ -15,18 +14,16 @@ int main() {
 
     sort(v.begin(), v.end());
 
-    for(int i=1; i<=N; i++) {
-        dp[i] = 1;
+    int dp[104];
+    dp[1] = 1;
 
-        for(int j=i-1; j>=1; j--) {
-            if(v[i].first >= v[j].first && v[i].second >= v[j].second ) {
-                dp[i] = max(dp[i], dp[j]+1);
-            }
+    for(int i=2; i<=N; i++) {
+        dp[i] = 1;
+        for(int j=1; j<i; j++) {
+            if(v[i].second > v[j].second) dp[i] = max(dp[i], dp[j]+1);
         }
     }
 
     cout<<N - *max_element(dp+1, dp+N+1);
-
-
-
+    return 0;
 }
