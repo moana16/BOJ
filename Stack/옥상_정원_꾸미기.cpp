065ae763c20bdc07a79d36f66c33dc878ad6f24d
jimin <moana16@naver.com>
2025-04-09 14:54:21 +0900

    0409

--- Stack/옥상_정원_꾸미기.cpp
+++ Stack/옥상_정원_꾸미기.cpp
@@ -1,20 +1,20 @@
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
 using namespace std;
 
-//스택에 들어온 건물중 벤치마킹 할 수 있는 건물의 수를 더하는 것으로 풀음..
-
 int main() {
-    int n; cin>>n;
-    int cnt = 0;
-    stack<long long> s;
-    for(int i=1; i<=n; i++) {
-        long long h; cin>>h;
-        while(!s.empty() && s.top() <= h ) {
-            s.pop();
-        }
-        s.push(h);
-        cnt += s.size() -1;
+    int N; cin>>N;
+    stack<int> st;
+    long long cnt = 0;
 
+    for(int i=1; i<=N; i++){
+        int h; cin>>h;
+
+        while(!st.empty() && st.top() <= h) {
+            st.pop();
+        }
+
+        cnt += st.size();
+        st.push(h);
     }
     cout<<cnt;
 }
